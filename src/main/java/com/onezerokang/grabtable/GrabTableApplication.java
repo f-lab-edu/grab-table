1eea360c048d718a65f3d0bf6d8b941482c696a5
Wonyoung Kang <onezerokang@gmail.com>
2024-01-01 19:05:40 +0900

    [#2] 프로젝트 생성

---
+++ src/main/java/com/onezerokang/grabtable/GrabTableApplication.java
@@ -0,0 +1,13 @@
+package com.onezerokang.grabtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onezerokang/grabtable/GrabtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onezerokang.grabtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
