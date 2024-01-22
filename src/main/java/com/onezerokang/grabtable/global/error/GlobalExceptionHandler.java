d78d41165cb7ace715011b1929ffa481e6cd97c5
Wonyoung Kang <onezerokang@gmail.com>
2024-01-22 13:32:51 +0900

    [#2] 전역 예외 핸들러 추가

---
+++ src/main/java/com/onezerokang/grabtable/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.onezerokang.grabtable.global.error;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
