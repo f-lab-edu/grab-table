e34845cda776a887e35a8c23e8143f9558fd6e63
Wonyoung Kang <onezerokang@gmail.com>
2024-01-29 12:53:30 +0900

    [#2] 일관된 예외 처리를 위해 ApiException과 ErrorResponse 구현

---
+++ src/main/java/com/onezerokang/grabtable/global/common/ErrorResponse.java
@@ -0,0 +1,37 @@
+package com.onezerokang.grabtable.global.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int httpStatus;
+    private final String message;
+    private final String errorCode;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<ValidationError> errors;
+
+    @Getter
+    @Builder
+    @RequiredArgsConstructor
+    public static class ValidationError {
+        private final String field;
+        private final String message;
+
+        public static ValidationError of(final FieldError fieldError) {
+            return ValidationError.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build();
+        }
+    }
+}
--- src/main/java/com/onezerokang/grabtable/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/onezerokang/grabtable/global/error/GlobalExceptionHandler.java
@@ -1,8 +1,34 @@
 package com.onezerokang.grabtable.global.error;
 
+import com.onezerokang.grabtable.global.common.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+import static com.onezerokang.grabtable.global.common.ErrorResponse.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ErrorResponse> handleApiException(ApiException e) {
+        return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getHttpStatus().value(), e.getMessage(), e.getErrorCode()));
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
+        String message = "Internal server error";
+        String errorCode = "InternalServerError";
+        return ResponseEntity.status(500).body(new ErrorResponse(500, message, errorCode));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<ValidationError> errors = e.getBindingResult().getFieldErrors().stream().map(ValidationError::of).toList();
+        String errorCode = e.getClass().getSimpleName().replace("Exception", "");
+        String message = "잘못된 매개변수입니다.";
+        return ResponseEntity.status(e.getStatusCode()).body(new ErrorResponse(e.getStatusCode().value(), message, errorCode, errors));
+    }
 }
