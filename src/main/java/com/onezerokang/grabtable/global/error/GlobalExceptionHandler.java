072f4703e0f177ab4c2a68c055ec82792d99d528
Wonyoung Kang <onezerokang@gmail.com>
2024-02-04 18:08:38 +0900

    [#2] GlobalExceptionHandler에 로거 추가

--- src/main/java/com/onezerokang/grabtable/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/onezerokang/grabtable/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.onezerokang.grabtable.global.error;
 
 import com.onezerokang.grabtable.global.common.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,14 +12,17 @@ import java.util.List;
 import static com.onezerokang.grabtable.global.common.ErrorResponse.*;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(ApiException e) {
+        log.error("handleApiException", e);
         return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getHttpStatus().value(), e.getMessage(), e.getErrorCode()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
+        log.error("handleUnexpectedException", e);
         String message = "Internal server error";
         String errorCode = "InternalServerError";
         return ResponseEntity.status(500).body(new ErrorResponse(500, message, errorCode));
@@ -26,6 +30,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
         List<ValidationError> errors = e.getBindingResult().getFieldErrors().stream().map(ValidationError::of).toList();
         String errorCode = e.getClass().getSimpleName().replace("Exception", "");
         String message = "잘못된 매개변수입니다.";
